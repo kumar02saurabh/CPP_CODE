9ae80aafd93be24177cc6b716fb17dbeb19109c2
Saurabh kumar <124427550+kumar02saurabh@users.noreply.github.com>
2024-01-31 22:55:02 +0530

    Add files via upload
    In this file I have Write a two digit sum code and some basic of TypeCasting

---
+++ second.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int a,b;
+    cout<<"Enter Your First number";
+    cin>>a;
+    cout<<"Enter your Second Number";
+    cin>>b;
+    cout<<"Answer:";
+    cout<<a+b;
+
+    // cout<<a-b;
+    // cout<<a*b;
+    // cout<<a/b;
+        }
+    
+        // TYPE CASTING
+// int a= 10;
+// char c= 'b';
+// a=c;
+// cout<<a;
+
+// int a= 75;
+// char c='b';
+// c=a;
+// cout<<c;
+
+// }
