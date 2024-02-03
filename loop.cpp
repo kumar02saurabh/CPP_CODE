835055a35883d1057590a143529b3687ebfdcf9e
Saurabh kumar <124427550+kumar02saurabh@users.noreply.github.com>
2024-02-03 20:11:19 +0530

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for ( int i=1;i<10;i++ )
+    {
+        cout <<"Hello\n";
+    }
+}
+
+
+
+
+                // n natural number
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number";
+    cin>>n;
+    for ( int count =1;count <=n; count++ )
+    {
+        cout <<count<<endl;
+    }
+}
+
+            // Square of Numbers
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the number";
+//     cin>>n;
+//     for (int count=1;count<=n; count =count+1)
+//     {
+//         cout<<count<<" Square is:"<<count*count<<endl;
+//     }
+
+// }
+
+
+            // Number between 100 and 200
+
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     int n;
+//     cout<<"Numbers are\n";
+//     for (int i =100; i<=200; i=i+1)
+//     cout<<i<<endl;
+//     } 
+
+                    // Even number 
+
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     int n;
+
+//     cout<<"Even Numbers are:\n";
+//     cin>>n;
+//     for (int i=2; i<=n; i=i+2)
+//     cout<<i<<endl;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter The Numbers ";
+//     cin>>n;
+//     for(int i=1;i<20;i=i+4)
+//     cout<<i<<" ";
+// }
+
+
+                // Odd Number 
+
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     int num;
+//     cout <<"Enter the number ";
+//     cin>>num;
+//     for (int i=1; i<=num;i=i+1){
+//     if(i%2!=0){
+//         cout<<i<<" ";
+//     }
+//     }
+    
+// }
+
