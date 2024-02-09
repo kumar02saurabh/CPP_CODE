1f202ace52712146ceee1f1b67347a0e87735bf0
Saurabh kumar <124427550+kumar02saurabh@users.noreply.github.com>
2024-02-09 21:24:55 +0530

    Add files via upload

---
+++ StarPattern2.cpp
@@ -0,0 +1,140 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int row, col;
+//     for(row=1;row<=5;row =row+1)
+//     {
+//         for (col=1; col<=row;col=col+1)
+//        {
+//         cout<<"*"<<" ";
+//         } 
+//         cout<<endl;
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int row,col;
+//     for(row=1;row<=5;row=row+1)
+//     {
+//         for(col=1;col<=row;col=col+1)
+//         {
+//         cout<<col<<" ";
+//     }
+//         cout<<endl;
+//     }
+    
+
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int row,col;
+//     for(row=1;row<=5;row=row+1)
+//     {
+//         for(col=1;col<=row;col=col+1)
+//         {
+//             cout<<row<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int row,col;
+//     for(row=1;row<=5;row=row+1)
+//     {
+//         for(col=row;col>=1;col= col-1)
+//         {
+//             cout<<col<<" ";
+//         }
+//         cout<<endl;
+//     }
+
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int row,col;
+//     for(row=1;row<=5;row=row+1)
+    
+//     {
+//         char name='a'+row-1;
+//         for(col=1;col<=row;col=col+1)
+//         {
+//             // char name='a'+row-1; yaha Bhi likh Sakte hai 
+//             cout<<name <<" ";
+//         }
+//         cout<<endl;
+
+//     }
+
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int row, col;
+//     for (row=1;row<=5;row=row+1)
+//     {
+//         for(col=1;col<=5-(row-1);col=col+1)
+//         {
+//             cout<<"*"<<"  ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int row,col;
+//     for (row=1;row<=5;row=row+1)
+//     {
+//         for(col=1;col<=5-(row-1);col=col+1)
+//         {
+//             cout<<col<<" ";
+//         }
+//         cout<<endl;
+
+      
+        
+//     }
+// }
+    
+
+    #include<iostream>
+    using namespace std;
+    int main(){
+        int row,col;
+        for(row=1;row<=5;row=row+1)
+        {
+            for(col=5;col>=5-(row-1);col=col-1)
+            {
+                cout<<col<<" ";
+            }
+            cout<<endl;
+        }
+    }
+        
+
+       
+    
+
+
+
