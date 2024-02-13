4b3ed75a327b63adac50c48dc72a22c4af0bc3b0
Saurabh kumar <124427550+kumar02saurabh@users.noreply.github.com>
2024-02-13 22:15:14 +0530

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<15/2;
+}
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<12.23/2;
+}
+
+
+#include<iostream>
+using namespace std;
+int main (){
+    int a,b;
+    cout<<"Entrer The Value of a:";
+    cin>>a;
+    cout<<"Enter The Value of b:";
+    cin>>b;
+//    ==
+    // if (a==b)
+    // cout<<"Yes";
+    // else 
+    // cout<<"NO";
+
+    // >
+    // if (a>b)
+    // cout<<"Yes";
+    // else 
+    // cout<<"NO";
+
+//  <
+    //  if (a<b)
+    // cout<<"Yes";
+    // else 
+    // cout<<"NO";
+
+        // !=
+      if (a!=b)
+    cout<<"Yes";
+    else 
+    cout<<"NO";
+
+}
+
+
+#include<iostream>
+using namespace std;
+int main(){
+
+
+
+    //  AND Operator 
+    // int a,b,c;
+   
+    // cin>>a>>b>>c;
+    //  if (a>b &&a>c)
+    //  cout<<"Yes";
+    //  else 
+    //  cout<<"No";
+
+
+    // OR Operator 
+
+    char name ='e';
+    if (name =='a'||name =='e'||name =='i'||name =='o'||name =='u' )
+    cout<<"Vowel";
+    else 
+    cout<<"Consonent";
+
+}
